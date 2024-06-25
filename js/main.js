b1d039c6683f62ce2177e2728bb797dd7778636d
Sp0nch0135 <schoppdiego5@hotmail.com>
2024-06-25 23:34:07 +0000

    favorite animal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
+        window.alert("SABAS PLS LET ME SQUAT IM ON MY KNEES");
     });
 });
